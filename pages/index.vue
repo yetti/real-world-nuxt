0ea5c379aee7a036e8a3a9f06e85f32ad0496c31
Yetrina Battad <hello@yetti.io>
2020-07-28 20:57:36 +1000

    chore(deploy): initial commit

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    EventCard,
+  },
+  async asyncData({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events events at this time',
+      })
+    }
+  },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
+  head() {
+    return {
+      title: 'Event Listing',
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
